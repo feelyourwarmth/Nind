a5b29c41edabcf7c64f68aa1539840b04f583b77
feelyourwarmth <pientpvp@gmail.com>
2026-08-03 10:34:42 -0300

    first commit

---
+++ config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # eae
+end
+
+starship init fish | source
+set fish_greeting ""
+
+alias vencord='sh -c "$(curl -sS https://vencord.dev/install.sh)"'
+alias mtx='unimatrix -n -a -l k'
